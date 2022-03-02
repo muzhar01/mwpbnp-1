61915487099d3123e14be7766ffbb2efbc235939
yasir.rehman <yasir.rehman@nxb.com.pk>
2022-03-02 15:24:04 +0500

    Bug fixing

--- src/protected/modules/administrator/modules/domains/controllers/DefaultController.php
+++ src/protected/modules/administrator/modules/domains/controllers/DefaultController.php
@@ -83,10 +83,10 @@ class DefaultController extends Controller
     }
 
     private  function randomPassword() {
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+{}|><?~';
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
         $pass = array(); //remember to declare $pass as an array
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-        for ($i = 0; $i < 14; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $n = rand(0, $alphaLength);
             $pass[] = $alphabet[$n];
         }
