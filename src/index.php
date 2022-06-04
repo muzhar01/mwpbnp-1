1b7ef85196de163cfe5536ef5b1f4eb0cd03e694
yasir.rehman <yasir.rehman@nxb.com.pk>
2022-06-04 13:33:24 +0500

    Bug fixing in Home Page

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@ $yii = dirname ( __FILE__ ) . '/framework/yii.php';
 $server_name = str_replace('www.', '', $_SERVER['SERVER_NAME']);
 //die($server_name);
 // remove the following lines when in production mode
-defined ( 'YII_DEBUG' ) or define ( 'YII_DEBUG', false );
+defined ( 'YII_DEBUG' ) or define ( 'YII_DEBUG', true );
 // specify how many levels of call stack should be shown in each log message
 defined ( 'YII_TRACE_LEVEL' ) or define ( 'YII_TRACE_LEVEL', 3 );
 
